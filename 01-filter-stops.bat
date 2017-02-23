839086dfbc6db54d8fa4151e82060c4dcc989c44
highsource <aleksei.valikov@gmail.com>
2017-02-23 18:07:37 +0100

    Initial commit

---
+++ 01-filter-stops.bat
@@ -0,0 +1,7 @@
+psql^
+ --username=postgres^
+ --dbname VG^
+ -f 01-filter-stops.sql^
+ --set=stops_input="'%cd%\unfiltered-stops.txt'"^
+ --set=stops_output="'%cd%\stops.txt'"^
+ --set=selected_rs="'10041,10042,10043,10044,10045,10046'"
